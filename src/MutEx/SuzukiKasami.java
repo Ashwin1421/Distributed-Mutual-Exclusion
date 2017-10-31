014e8a5ec2f5bea6d11c898f7ffdd78af7bd46c6
Ashwin <ashwin.joshi12@live.com>
2017-10-31 13:15:15 -0500

    Modified SuzukiKasami.java

--- src/MutEx/SuzukiKasami.java
+++ src/MutEx/SuzukiKasami.java
@@ -5,10 +5,16 @@
  */
 package MutEx;
 
+import Message.Message;
 import Utilities.Utils;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Queue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,86 +29,132 @@ public class SuzukiKasami {
     int pid;
     boolean token = false;
     boolean insideCS = false;
-    Map<PrintWriter, Integer> PROCESS_LIST;
+    boolean requesting = false;
+    Integer executionCount = 1;
+    long t1 = 0;
+    long t2 = 0;
+    Map<ObjectOutputStream, Integer> PROCESS_LIST;
+    Queue<Integer> REQUEST_Q = new LinkedList<>();
     Utils prop = new Utils();
     
-    public SuzukiKasami(int pid, Map<PrintWriter, Integer> PROCESS_LIST){
+    public SuzukiKasami(int pid, Map<ObjectOutputStream, Integer> PROCESS_LIST){
         this.PROCESS_LIST = PROCESS_LIST;
         this.N = PROCESS_LIST.size();
         this.pid = pid;
-        this.RN = new Integer[N+1];
-        this.LN = new Integer[N+1];
+        this.RN = new Integer[N+2];
+        this.LN = new Integer[N+2];
         Arrays.fill(RN, 0);
         Arrays.fill(LN, 0);
+        if(pid == 1){
+            token = true;
+        }
     }
     
+    public void print(String s){
+        System.out.println("[Suzuki-Kasami-MutEx]$:"+s);
+    }
     public boolean hasToken(){
         return token;
+    }
+    
+    public void sendTo(Integer pid, Message msg){
+        for(ObjectOutputStream out : PROCESS_LIST.keySet()){
+            if(PROCESS_LIST.get(out).equals(pid)){
+                try {
+                    out.writeObject(msg);
+                    out.flush();
+                    print("SENT:"+msg.toString());
+                } catch (IOException ex) {
+                    Logger.getLogger(SuzukiKasami.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+    
+    public long getExecutionTime(){
+        return t1;
+    }
+    
+    public long getReleaseTime(){
+        return t2;
+    }
+    
+    public long getSynchDelay(long t1, long t2){
+        return t1 > t2 ? t1-t2 : t2-t1;
+    }
+    
+    public void addRequest(int pid){
+        REQUEST_Q.add(pid);
+    }
+    
+    public void delRequest(int pid){
+        REQUEST_Q.remove(pid);
+    }
+    
+    public Integer nextRequest(){
+        return REQUEST_Q.peek();
     }
     
     public void setToken(boolean token){
         this.token = token;
     }
     public void grantToken(int j){
-        String msg = "GRANT_CS";
-        for(PrintWriter out: PROCESS_LIST.keySet()){
-            if(PROCESS_LIST.get(out).equals(j)){
-                out.println(msg);
-                System.out.println("SENT="+msg);
+        Message sendMsg = new Message(pid);
+        sendMsg.setText("PRIVILEGE");
+        sendMsg.setQ(REQUEST_Q);
+        sendMsg.setLN(LN);
+        sendTo(j, sendMsg);
+    }
+    public void requestCS(int pid){
+        if(!hasToken()){
+            requesting = true;
+            RN[pid]++;
+            Message sendMsg = new Message(pid);
+            sendMsg.setText("REQUEST");
+            sendMsg.setseqno(RN[pid]);
+            for(Integer j: PROCESS_LIST.values()){
+                if(j!=pid){
+                    sendTo(j, sendMsg);
+                }
             }
         }
     }
-    public void requestCS(int i){
-        /**
-         * Requesting to enter CS.
-         * Increment RN for process i,
-         * and then send request messages
-         * to all other processes.
-         **/
-        if(!token){
-            RN[i]++;
-            /**
-             * Sequence number.
-             **/
-        }
-        String msg = "REQUEST_CS="+i+";"+RN[i];
-        for(PrintWriter output: PROCESS_LIST.keySet()){
-            output.println(msg);
-            System.out.println("SENT="+msg);
-        }
-    }
-    public void receiveCSRequest(int j, int seqNo){
+    public void receiveCSRequest(Integer j, Integer seqNo){
         
         RN[j]=Integer.max(RN[j], seqNo);
-        if(hasToken() && !insideCS && (RN[j]==(LN[j]+1))){
-            grantToken(j);
+        if(hasToken() && !requesting && (RN[j]==(LN[j]+1))){
             setToken(false);
+            grantToken(j);
         }
     }
-    public void releaseCS(int index){
-        /**
-         * Sending a release message to other processes.
-         **/
-        LN[index] = RN[index];
-        for(PrintWriter out: PROCESS_LIST.keySet()){
-            out.println("RELEASE_CS="+index);
+    public void releaseCS(Integer i){
+
+        LN[i] = RN[i];
+        for(Integer j : PROCESS_LIST.values()){
+            if(!Objects.equals(j, i)){
+                if(!REQUEST_Q.contains(j) && (RN[j]==(LN[j]+1))){
+                    REQUEST_Q.add(j);
+                }
+            }
         }
-        
+        if(!REQUEST_Q.isEmpty()){
+            token = false;
+            grantToken(REQUEST_Q.poll());
+        }
+        requesting = false;
     }
     
     public void executeCS(){
         
-        long t = prop.t3;
-        System.out.println("Executing CS for "+t+" millis.");
+        print("Executing CS for "+prop.t3+" ms."+"["+executionCount+"]");
+        executionCount++;
+        t1 = System.currentTimeMillis();
         try {
-            for(int i=1;i<=t;i++){
-                Thread.sleep(i);
-                System.out.print("...");
-            }
-            System.out.println();
+            Thread.sleep(prop.t3);
         } catch (InterruptedException ex) {
             Logger.getLogger(SuzukiKasami.class.getName()).log(Level.SEVERE, null, ex);
         }
+        t2 = System.currentTimeMillis();
     }
     
 }
